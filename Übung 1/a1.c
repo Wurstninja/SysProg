4130b2afeeb4cab898be015ec71b7f93850b2843
Maurice <s6mahapp@uni-bonn.de>
2018-10-16 18:58:25 +0200

    commit msg
    jajaja

--- Übung 1/a1.c
+++ Übung 1/a1.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef struct node
+{
+    struct node* head;
+    struct node* next;
+    struct node* prev;
+    int data;
+}node;
+
+node* add(node* head, int data);
+void printList(node*);
 int main()
 {
+    node* new=malloc(sizeof(node));
+    new=NULL;
+    new = add(new,1);
+    new = add(new,2);
+    new = add(new,3);
     return 0;
+}
+
+node* add(node* head, int data)
+{
+    if(head==NULL)
+    {
+        head->data=data;
+        head->next=NULL;
+    }
+    else
+    {
+        node* cur;
+        cur=malloc(sizeof(node));
+        head->next=cur;
+        printf("pre\n")
+        while(cur!=NULL)
+        {
+            printf("in\n");
+            cur->next=cur;
+        }
+        cur->data=data;
+        cur->next=NULL;
+    }
+    return head;
+}
+
+void printList(node* head)
+{
+    node* cur;
+    while (cur!=NULL)
+    {
+        printf("%i\n",cur->data);
+    }
 }
