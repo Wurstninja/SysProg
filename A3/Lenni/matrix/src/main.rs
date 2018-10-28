e52b6e318a85c0bcfe76b588dbc816e050aa0a6a
Lenni Hein <computerscience@uni-bonn.de>
2018-10-28 18:39:19 +0100

    A3: updated readme accordingly to codebase

--- A3/Lenni/matrix/src/main.rs
+++ A3/Lenni/matrix/src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
 
-fn main() 
+fn main()
 {
     let args: Vec<String> = env::args().collect();
-    if args.len() !=4 {panic!("Fuck");}
+    if args.len() != 4
+    {
+        panic!("Fuck");
+    }
 
     let mat1: Vec<Vec<i64>> = read_mat(args[1].as_str());
     let mat2: Vec<Vec<i64>> = read_mat(args[3].as_str());
@@ -16,13 +19,13 @@ fn main()
     {
         "+" => mat3 = mat_add(&mat1, &mat2),
         "-" => mat3 = mat_sub(&mat1, &mat2),
-        _   => panic!("Fuck"),
+        _ => panic!("Fuck"),
     }
 
     print_mat(&mat3);
 }
 
-fn read_mat(s: & str) -> Vec<Vec<i64>>
+fn read_mat(s: &str) -> Vec<Vec<i64>>
 {
     let mut mat: Vec<Vec<i64>> = Vec::new();
     let rows = s.split(";");
@@ -30,7 +33,7 @@ fn read_mat(s: & str) -> Vec<Vec<i64>>
     for (i, r) in rows_iter
     {
         mat.push(Vec::new());
-        let col = r.split(",");        
+        let col = r.split(",");
         for c in col
         {
             mat[i].push(c.parse::<i64>().unwrap());
@@ -42,9 +45,15 @@ fn read_mat(s: & str) -> Vec<Vec<i64>>
 fn mat_add(m1: &Vec<Vec<i64>>, m2: &Vec<Vec<i64>>) -> Vec<Vec<i64>>
 {
     let num_rows = m1.len();
-    if num_rows != m2.len() {panic!("ERR");}
+    if num_rows != m2.len()
+    {
+        panic!("ERR");
+    }
     let num_cols = m1[0].len();
-    if num_cols != m2[0].len() {panic!("ERR");}    
+    if num_cols != m2[0].len()
+    {
+        panic!("ERR");
+    }
 
     let mut mat: Vec<Vec<i64>> = Vec::with_capacity(num_rows);
 
